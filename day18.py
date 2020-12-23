0aee33e4ea8b078cbd2e1ded2de88a5bb257b854
Graham Keeth <graham.keeth@gmail.com>
2020-12-23 14:04:24 -0500

    add day18 part2 solution

--- day18.py
+++ day18.py
@@ -2,7 +2,13 @@
 import re
 
 class MathHomework(object):
-    def __init__(self, input_filename):
+    def __init__(self, input_filename, precedence="equal"):
+        if precedence == "equal":
+            self.precedence = {"+": 0, "*": 0}
+        elif precedence == "addition":
+            # addition is higher precedence than multiplication
+            self.precedence = {"+": 1, "*": 0}
+
         with open(input_filename, "r") as infile:
             self.expressions = [line.strip() for line in infile]
 
@@ -45,47 +51,53 @@ class MathHomework(object):
         return sum([self.evaluate_expression(expression)
             for expression in self.expressions])
 
-    def evaluate_expression(self, expression):
+    def parse_expression(self, expression):
+        """
+        parse untokenized expression into tokenized RPN expression (list) that
+        can be evaluated
+
+        (shunting-yard algorithm)
+        """
+        output = []
+        operators = []
         tokenized = self.tokenize(expression)
-        # print(tokenized)
-        a = 0
-        op = ""
-        stored = []
         for token in tokenized:
             if re.match("^\d+$", token):
-                if op == "":
-                    a = int(token)
-                elif op == "*":
-                    a *= int(token)
-                    op = ""
-                    # print(a)
-                elif op == "+":
-                    a += int(token)
-                    op = ""
-                    # print(a)
-            elif re.match("^[+*]$", token):
-                op = token
+                output.append(token)
+            elif re.match("^[+*]", token):
+                while (operators
+                        and operators[-1] != "("
+                        and self.precedence[operators[-1]] >= self.precedence[token]):
+                    output.append(operators.pop())
+                operators.append(token)
             elif re.match("^\($", token):
-                stored.append((a, op))
-                a = 0
-                op = ""
+                operators.append(token)
             elif re.match("^\)$", token):
-                temp = stored.pop()
-                if temp[1] == "*":
-                    a = temp[0] * a
-                    op = ""
-                    # print(a)
-                elif temp[1] == "+":
-                    a = temp[0] + a
-                    op = ""
-                    # print(a)
-                elif temp[1] == "":
-                    op = ""
-        print(f"total: {a}")
-        return a
+                while operators and operators[-1] != "(":
+                    output.append(operators.pop())
+                if operators and operators[-1] == "(":
+                    operators.pop()
+        # put everything remaining in operator queue into output
+        while operators:
+            output.append(operators.pop())
+        return output
+
+    def evaluate_expression(self, expression):
+        rpn = self.parse_expression(expression)
+        stack = []
+        for token in rpn:
+            if re.match("^\d+$", token):
+                stack.append(int(token))
+            elif re.match("^\+$", token):
+                stack.append(stack.pop() + stack.pop())
+            elif re.match("^\*$", token):
+                stack.append(stack.pop() * stack.pop())
+        return stack.pop()
 
 if __name__ == "__main__":
     h = MathHomework("inputs/day18_test.txt")
     print(f"day18 test: {h.do_homework()}")
     h = MathHomework("inputs/day18.txt")
     print(f"day18 part1: {h.do_homework()}")
+    h = MathHomework("inputs/day18.txt", precedence="addition")
+    print(f"day18 part2: {h.do_homework()}")
