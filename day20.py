59efd1a23f59b075eadff8eea5497a82f30a0de5
Graham Keeth <graham.keeth@analog.com>
2021-01-05 15:36:12 -0500

    complete day20b

--- day20.py
+++ day20.py
@@ -3,6 +3,7 @@ from itertools import combinations
 from functools import reduce
 from collections import defaultdict
 import math
+import re
 
 class Tile(object):
     def __init__(self, id_num, data):
@@ -89,7 +90,27 @@ class TileMap(object):
     def multiply_corner_ids(self):
         return reduce((lambda a, b: a * b), self.find_corner_tiles())
 
-    def assemble_image(self):
+    @staticmethod
+    def rotate(m):
+        """ rotate map 90deg clockwise """
+        rotated = []
+        for n in range(len(m)):
+            rotated.append("".join([row[n] for row in m[::-1]]))
+        return rotated
+
+    @staticmethod
+    def flipx(m):
+        """ flip map vertically (mirror across x axis) """
+        return m[::-1]
+
+
+    def measure_roughness(self):
+        """
+        1. assemble tiles into map
+        2. strip edges
+        3. count sea monsters in map
+        4. measure roughness of seas
+        """
         row_len = int(math.sqrt(len(self.tiles)))
         m = [[None for _ in range(row_len)] for _ in range(row_len)]
         corners = self.find_corner_tiles()
@@ -126,22 +147,57 @@ class TileMap(object):
                             if "left" not in matching_edges and "top" in matching_edges and matching_edges["top"] == upper.id_num:
                                 break
                         else:
-                            if "left" in matching_edges and matching_edges["left"] == prev.id_num and matching_edges["top"] == upper.id_num:
+                            if ("left" in matching_edges and matching_edges["left"] == prev.id_num
+                                    and "top" in matching_edges and matching_edges["top"] == upper.id_num):
                                 break
                     t()
                     # transforming matching_edges would be cheaper than recalculating...
                     matching_edges = tile.get_matching_edges(self.tiles.values())
                 m[row][n] = tile.id_num
-        print(m)
-        stripped_m = [list(map("".join, zip(*[self.tiles[id_num].strip_edges() for id_num in row]))) for row in m]
-        print(stripped_m)
+
+        m = [list(map("".join, zip(*[self.tiles[id_num].strip_edges() for id_num in row]))) for row in m]
+        m = [col for block in m for col in block]
+
+        # search for sea creatures
+        # sea creatures look like:
+        # __________________#_
+        # #____##____##____###
+        # _#__#__#__#__#__#___
+        #
+        # sea creatures contain 15 '#'es
+        monster1 = "..................#."
+        monster1 = "#" # preceded by 18 chars, succeeded by 1
+        monster2 = "#....##....##....###"
+        monster3 = ".#..#..#..#..#..#..."
+        transforms = [self.rotate, self.rotate, self.rotate, self.flipx, self.rotate, self.rotate, self.rotate, lambda a: a]
+        for t in transforms:
+            monsters = 0
+            for l, line in enumerate(m):
+                for match1 in re.finditer(monster1, line[18:len(m)-1]):
+                    if l + 2 < len(m):
+                        prefix = "." * match1.start()
+                        match2 = re.match(prefix + monster2, m[l+1])
+                        if match2:
+                            match3 = re.match(prefix + monster3, m[l+2])
+                            if match3:
+                                monsters += 1
+            if monsters > 0:
+                break
+            else:
+                m = t(m)
+
+        # count number of '#' chars in m, and subtract those contained in
+        # monsters to measure roughness of sea
+        return "".join(m).count("#") - monsters * 15
+
+
 
 
 if __name__ == "__main__":
     t = TileMap("inputs/day20_test.txt")
     print(f"day 20 test: {t.multiply_corner_ids()}")
-    print(f"matching edges in 2311: {t.tiles[2311].get_matching_edges(t.tiles.values())}")
-    t.assemble_image()
+    print(f"day 20b test: {t.measure_roughness()}")
     t = TileMap("inputs/day20.txt")
     print(f"day 20a: {t.multiply_corner_ids()}")
+    print(f"day 20b: {t.measure_roughness()}")
 
