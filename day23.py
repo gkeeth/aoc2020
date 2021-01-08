8c8ce127ce7c5d7b617b53385bf63558b00ecd46
Graham Keeth <graham.keeth@analog.com>
2021-01-08 16:20:49 -0500

    complete day23

--- day23.py
+++ day23.py
@@ -2,65 +2,64 @@
 
 class Game(object):
     def __init__(self, input_string, version=1):
-        self.cups = [int(n) for n in input_string]
-        self.current = 0 # index of current cup
-        self.version = version
+        cups = [int(n) for n in input_string]
         if version == 2:
-            # add additional numbers up to 1 million
-            self.cups += range(max(self.cups), 1000001)
+            cups.extend(range(max(cups) + 1, 1000001))
+        self.cups = {cups[n]: cups[(n+1) % len(cups)] for n in range(len(cups))}
+        self.current = cups[0]
+        self.version = version
+        self.min = min(cups)
+        self.max = max(cups)
+
+    def print_cups(self):
+        c = []
+        n = self.current
+        for _ in range(len(self.cups)):
+            c.append(str(n))
+            n = self.cups[n]
+        print(" ".join(c))
+
 
     def play_move(self):
-        # import pdb; pdb.set_trace()
-        # print(self.cups)
-        # print(f"current index: {self.current}")
-        # print(f"current: {self.cups[self.current]}")
-        current_val = self.cups[self.current]
         # remove the 3 cups after current
-        removed = []
-        for _ in range(3):
-            index = self.cups.index(current_val)
-            removed.append(self.cups.pop((index + 1) % len(self.cups)))
-        # print(f"pickup: {removed}")
+        cup1 = self.cups[self.current]
+        cup2 = self.cups[cup1]
+        cup3 = self.cups[cup2]
+        removed = [cup1, cup2, cup3]
+        self.cups[self.current] = self.cups[cup3]
 
         # pick a destination for removed cups
-        dest = self.cups[self.cups.index(current_val)] - 1
-        if dest < min(self.cups + removed):
-            dest = max(self.cups + removed)
+        dest = self.current - 1
+        if dest < self.min:
+            dest = self.max
         while dest in removed:
             dest -= 1
-            if dest < min(self.cups + removed):
-                dest = max(self.cups + removed)
-        dest_index = self.cups.index(dest)
-        # print(f"destination: {dest} at {dest_index}")
+            if dest < self.min:
+                dest = self.max
 
         # insert the 3 removed cups after dest_index
-        for n, cup in enumerate(removed):
-            self.cups.insert(dest_index + 1 + n, cup)
+        self.cups[cup3] = self.cups[dest]
+        self.cups[dest] = cup1
 
         # choose new current cup
-        self.current = (self.cups.index(current_val) + 1) % len(self.cups)
-
-    @staticmethod
-    def rotate(cups):
-        """ right-shift list of cups and return rotated list """
-        return cups[-1:] + cups[:-1]
+        self.current = self.cups[self.current]
 
     def play(self, rounds=100):
         for n in range(rounds):
-            # print(f"-- move {n+1} --")
             self.play_move()
-            # print()
 
         if self.version == 1:
             # result should be formatted with the cups in order, starting after "1"
-            cups = [str(n) for n in self.cups]
-            while cups[0] != "1":
-                cups = Game.rotate(cups)
-            return "".join(cups[1:])
+            cups = []
+            n = self.cups[1]
+            for _ in range(len(self.cups) - 1):
+                cups.append(str(n))
+                n = self.cups[n]
+            return "".join(cups)
         elif self.version == 2:
-            index = self.cups.index(1)
-            return (self.cups[(index + 1) % len(self.cups)]
-                    * self.cups[(index + 2) % len(self.cups)])
+            print(f"cup 1: {self.cups[1]}")
+            print(f"cup 2: {self.cups[self.cups[1]]}")
+            return self.cups[1] * self.cups[self.cups[1]]
 
 if __name__ == "__main__":
     g = Game("389125467")
@@ -69,5 +68,7 @@ if __name__ == "__main__":
     print(f"day23a test2: {g.play(rounds=100)}")
     g = Game("653427918")
     print(f"day23a: {g.play(rounds=100)}")
+    g = Game("389125467", version=2)
+    print(f"day23b test: {g.play(rounds=10000000)}")
     g = Game("653427918", version=2)
     print(f"day23b: {g.play(rounds=10000000)}")
